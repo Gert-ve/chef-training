a5aaebb5c004eb313da8149d06b42114ac0b2607
Gert <gert.vanemmenes@travelstart.com>
2018-01-24 10:43:19 +0200

    Added comments and removed file

--- cookbooks/hello/recipes/default.rb
+++ cookbooks/hello/recipes/default.rb
@@ -4,12 +4,15 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
+# Updates the package manager repo
 apt_update
 
+# Installs nginx
 package 'nginx' do
   action :install
 end
 
+#Creates a file using a template
 template '/var/www/html/index.html' do
   source 'index.html.erb'
   mode '0755'

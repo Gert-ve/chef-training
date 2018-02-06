600345e73a4044abf77ef062623a98f8c969af4c
Gert <gert.vanemmenes@travelstart.com>
2018-02-06 12:36:50 +0200

    Update for training

--- cookbooks/hello/recipes/default.rb
+++ cookbooks/hello/recipes/default.rb
@@ -6,14 +6,3 @@
 
 # Updates the package manager repo
 apt_update
-
-# Installs nginx
-package 'nginx' do
-  action :install
-end
-
-#Creates a file using a template
-template '/var/www/html/index.html' do
-  source 'index.html.erb'
-  mode '0755'
-end
